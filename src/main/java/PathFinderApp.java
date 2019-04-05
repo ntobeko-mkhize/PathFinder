22a56947427167db22828fd86cfa6d58d9f3db4f
Mkhize Ntobeko <axx1390@africa.bmw.corp>
2019-04-05 06:07:21 +0200

    renamed main class file

--- src/main/java/PathFinderApp.java
+++ src/main/java/PathFinderApp.java
@@ -1,211 +1,170 @@
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.Scanner;
 
 /**
- * @developer ntobekomkhize@yahoo.com
+ * @author ntobekomkhize@yahoo.com || mkhizenj@gmail.com
  * <p>
- * Alt. Contact mkhizenj@gmail.com
  */
 public class PathFinderApp {
 
-    static String graph[][] = null;
-    static String graphResult[][] = null;
+    private static String graph[][] = null;
+    private static String graphResult[][] = null;
 
-    static int V = 39;
+    private static Vertex startVertex = null;
+    private static Vertex destinationVertex = null;
+
+    private static final int row[] = {-1, 0, 0, 1};
+    private static final int col[] = {0, -1, 1, 0};
 
     public static void main(String[] args) {
         System.out.println("The app was started");
 
         PathFinderApp pathFinderApp = new PathFinderApp();
-        //System.out.println(pathFinderApp.getFile("map.txt"));
+        pathFinderApp.getFile("map.txt");
+        pathFinderApp.findLocale();
+        pathFinderApp.findShortestPath_BFS();
+        Util.writeResultFiles(graph, graphResult);
 
-//        pathFinderApp.getFile("map.txt");
-        findShortestPath();
-        //  writeResultFile();
+        System.out.println("The app completed");
     }
 
-    private static void findShortestPath() {
+    private String getFile(String fileName) {
 
-        int breadth;
-//        for (int i = 0; i < graph.length; i++) {
-//            breadth = graph[0].length;
-//            System.out.println(graph[0][i]);
-//        }
+        //StringBuilder result = new StringBuilder("");
 
-        dijkstra(graph, 0);
-    }
+        //Get file from resources folder
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource(fileName).getFile());
 
-    private static int minDistance(int dist[], Boolean shortestPathTree[]) {
-        // Initialize min value
-        int min = Integer.MAX_VALUE, index = -1;
+        int height = 0;
+        int width = 0;
 
-        for (int v = 0; v < V; v++)
-            if (shortestPathTree[v] == false && dist[v] <= min) {
-                min = dist[v];
-                index = v;
+        try (Scanner scanner = new Scanner(file)) {
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                line = line.trim();
+                line = line.replaceAll("\\s+", "");
+                width = line.length();
+
+                height++;
             }
 
-        return index;
-    }
+            scanner.close();
 
-    private static void dijkstra(String graph[][], int src) {
-        int distance[] = new int[V]; // The output array. distance[i] will hold
-        // the shortest distance from src to i
-
-        // shortestPathTree[i] will true if vertex i is included in shortest
-        // path tree or shortest distance from src to i is finalized
-        Boolean shortestPathTree[] = new Boolean[V];
-
-        // Initialize all distances as INFINITE and shortestPathTree[] as false
-        for (int i = 0; i < V; i++) {
-            distance[i] = Integer.MAX_VALUE;
-            shortestPathTree[i] = false;
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-        // Distance of source vertex from itself is always 0
-        distance[src] = 0;
+        int currentLine = 0;
+        try (Scanner scanner = new Scanner(file)) {
 
-        // Find shortest path for all vertices
-        for (int vCount = 0; vCount < V - 1; vCount++) {
-            // Pick the minimum distance vertex from the set of vertices
-            // not yet processed. u is always equal to src in first
-            // iteration.
-            int u = minDistance(distance, shortestPathTree);
+            graph = new String[height][width];
+            graphResult = new String[height][width];
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                line = line.trim();
+                line = line.replaceAll("\\s+", "");
 
-            // Mark the picked vertex as processed
-            shortestPathTree[u] = true;
+                int charCount = 0;
+                for (int i = 0; i < line.length(); i++) {
+                    graph[currentLine][i] = "" + line.charAt(i);
+                    graphResult[currentLine][i] = "" + line.charAt(i);
+                    charCount++;
+                }
+                currentLine++;
+            }
 
-            // Update distance value of the adjacent vertices of the
-            // picked vertex.
-            for (int v = 0; v < V; v++) {
-//                System.out.println("vertex:: " + v);
-//                System.out.println("graphResult[u]:: " + graphResult[u]);
-//                graphResult[u][v] = "\"";
+            scanner.close();
 
-                // Update distance[v] only if is not in shortestPathTree, there is an
-                // edge from u to v, and total weight of path from src to
-                // v through u is smaller than current value of distance[v]
-//                if (!shortestPathTree[v] && graph[u][v] != "W" &&
-//                        distance[u] != Integer.MAX_VALUE ){
-//
-//                    //distance[v] = distance[u] + graph[u][v];
-//                    distance[v] = 0;
-//                    graphResult[u][v] = "*";
-//
-//                    if("E" == graph[u][v])
-//                        break;
-//                }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //return result.toString();
+        return "";
+
+    }
+
+    private static void findLocale() {
+
+        boolean start = false, destination = false;
+        for (int i = 0; i < graph.length; i++) {
+            for (int y = 0; y < graph[i].length; y++) {
+
+                if ("S".equals(graph[i][y])) {
+                    startVertex = new Vertex(i, y, 0);
+                    start = true;
+                }
+
+                if ("E".equals(graph[i][y])) {
+                    destinationVertex = new Vertex(i, y, 0);
+                    destination = true;
+                }
+
+                if (start && destination)
+                    break;
+            }
+        }
+    }
+
+    private static void findShortestPath_BFS() {
+        int i = startVertex.getY();
+        int j = startVertex.getX();
+        int a = destinationVertex.getY();
+        int b = destinationVertex.getX();
+
+        // create an empty queue
+        Queue<Vertex> q = new ArrayDeque<>();
+
+        q.add(new Vertex(i, j, 0));
+
+        // stores length of longest path from source to destination
+        int min_dist = Integer.MAX_VALUE;
+
+        // run till queue is not empty
+        while (!q.isEmpty()) {
+            // pop front vertex from queue and process it
+            Vertex vertex = q.poll();
+
+            // (i, j) represents current cell and dist stores its
+            // minimum distance from the source
+            i = vertex.getX();
+            j = vertex.getY();
+            int dist = vertex.getDist();
+
+            // if destination is found, update min_dist and stop
+            if (i == a && j == b) {
+                min_dist = dist;
+                break;
+            }
+
+            // check for all 4 possible movements from current cell
+            // and enqueue each valid movement
+            for (int k = 0; k < 4; k++) {
+                // check if it is possible to go to position
+                // (i + row[k], j + col[k]) from current position
+                if (moveToVertex(i + row[k], j + col[k])) {
+                    // mark next cell as visited and enqueue it
+                    graphResult[i + row[k]][j + col[k]] = "\"";
+                    q.add(new Vertex(i + row[k], j + col[k], dist + 1));
+                }
             }
         }
 
-        // print the constructed distance array
-        //printSolution(distance, V);
+        if (min_dist != Integer.MAX_VALUE) {
+            System.out.print("The shortest path from source to destination "
+                    + "has length " + min_dist);
+        } else {
+            System.out.print("Destination can't be reached from source");
+        }
     }
 
-//    private static void writeResultFile() {
-//        try {
-//            FileWriter fw = new FileWriter("output.txt");
-//
-//            // read character wise from string and write
-//            // into FileWriter
-//
-//            for (int i = 0; i < graphResult.length; i++) {
-//                for (int y = 0; y < graphResult[i].length; y++) {
-//                    fw.write(graphResult[i][y]);
-//                }
-//                fw.write("\n");
-//            }
-//
-//
-//            System.out.println("Writing successful");
-//            //close the file
-//            fw.close();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-
-//    private String getFile(String fileName) {
-//
-//        //StringBuilder result = new StringBuilder("");
-//
-//        //Get file from resources folder
-//        ClassLoader classLoader = getClass().getClassLoader();
-//        File file = new File(classLoader.getResource(fileName).getFile());
-//
-//        int lineCount = 0;
-//
-//        try (Scanner scanner = new Scanner(file)) {
-//
-//            while (scanner.hasNextLine()) {
-//                scanner.nextLine();
-//                lineCount++;
-//            }
-//
-//            scanner.close();
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//        int currentLine = 0;
-//        try (Scanner scanner = new Scanner(file)) {
-//
-//            graph = new String[lineCount][];
-//            graphResult = new String[lineCount][];
-//            while (scanner.hasNextLine()) {
-//                String line = scanner.nextLine();
-//                line = line.trim();
-//                line.replaceAll("\\s+","");
-//                int charCount = 0;
-//
-//                System.out.println("length: " + line.length());
-//                String lineItems[] = new String[line.length()];
-//                for (int i = 0; i < line.length(); i++) {
-//                    lineItems[charCount] = "" + line.charAt(i);
-//                    charCount++;
-//                }
-//                graph[currentLine] = lineItems;
-//                graphResult[currentLine] = lineItems;
-//                currentLine++;
-//                //result.append(line).append("\n");
-//            }
-//
-//            scanner.close();
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//        //return result.toString();
-//        return "";
-//
-//    }
-
-//    private String getFile(String fileName) {
-//
-//        StringBuilder result = new StringBuilder("");
-//
-//        //Get file from resources folder
-//        ClassLoader classLoader = getClass().getClassLoader();
-//        File file = new File(classLoader.getResource(fileName).getFile());
-//
-//        try (Scanner scanner = new Scanner(file)) {
-//
-//            while (scanner.hasNextLine()) {
-//                String line = scanner.nextLine();
-//                result.append(line).append("\n");
-//            }
-//
-//            scanner.close();
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//        return result.toString();
-//
-//    }
+    private static boolean moveToVertex(int row, int col) {
+        return (row >= 0) && (row < graphResult.length) && (col >= 0) && (col < graphResult[row].length)//maybe row -1
+                && !"W".equals(graph[row][col]) && !"\"".equals(graphResult[row][col]);
+    }
 }
--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -7,7 +7,25 @@ import java.io.IOException;
  */
 public class Util {
 
-    public static void writeResultFile(String graphResult[][]) {
+    public static void writeResultFiles(String graph[][], String graphResult[][]) {
+        try {
+            System.out.println("Writing to input.txt");
+            FileWriter fw = new FileWriter("input.txt");
+
+            for (int i = 0; i < graph.length; i++) {
+
+                for (int y = 0; y < graph[i].length; y++) {
+                    fw.write(graph[i][y] + " ");
+                }
+                fw.write("\n");
+            }
+
+            System.out.println("Writing input successful");
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         try {
             System.out.println("Writing to output.txt");
             FileWriter fw = new FileWriter("output.txt");
@@ -20,7 +38,7 @@ public class Util {
                 fw.write("\n");
             }
 
-            System.out.println("Writing successful");
+            System.out.println("Writing output successful");
             fw.close();
         } catch (IOException e) {
             e.printStackTrace();
